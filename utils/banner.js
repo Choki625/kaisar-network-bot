aedfbde4b4d9f3c381f23956121e8a04503e7168
Choki625 <muhammadhawi123@gmail.com>
2024-11-25 07:54:43 +0700

    Commit pertama

---
+++ utils/banner.js
@@ -0,0 +1,7 @@
+export const banner = `
+ ============================================
+|             Kaisar Network Bot             |
+|               Airdrop Vault                |
+|         https://t.me/airdropvault_id       |
+|             github.com/Choki625            |
+ =============================================`;
